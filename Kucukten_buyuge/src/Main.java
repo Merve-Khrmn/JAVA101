6dd77c3abe86434afe3565a55d345a2f2a5a85e3
Merve-Khrmn <102805651+Merve-Khrmn@users.noreply.github.com>
2022-08-17 14:18:05 +0300

    Add files via upload

---
+++ Kucukten_buyuge/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner k=new Scanner(System.in);
+        System.out.print("1.sayıyı giriniz:");
+        a=k.nextInt();
+        System.out.print("2.sayıyı giriniz:");
+        b=k.nextInt();
+        System.out.print("3.sayıyı giriniz:");
+        c=k.nextInt();
+        if(a<b && a<c){
+            if(b<c)
+            System.out.println("a<b<c");
+        else System.out.println("a<c<b");
+        }
+        if(b<a && b<c){
+            if(a<c)
+                System.out.println("b<a<c");
+            else System.out.println("b<c<a");}
+
+            if (c<a && c<b){
+                if(a<b)
+                    System.out.println("c<a<b");
+                else System.out.println("c<b<a");}
+    }
+
+}
