2336f6e5feb3cbfbbc9fa3cfcdefa1b63c0b0bf8
Merve Kahraman <102805651+Merve-Khrmn@users.noreply.github.com>
2023-09-01 02:02:06 +0300

    Update Main.java

--- hesap_makinesi/src/Main.java
+++ hesap_makinesi/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
             sayi2=k.nextInt();
         System.out.println("Lütfen yapmak istediğiniz işlemi seçiniz\n1-toplama\n2-çıkarma\n3-çarpma\n4-bölme");
             select=k.nextInt();
-        if(n2!=0){
+        if(sayi2!=0){
             switch (select){
                 case 1:
                     System.out.println("Toplam"+(sayi1+sayi2));
